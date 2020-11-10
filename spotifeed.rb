b37e9b29097514fd0e68e248a3d02b7462c307da
Ronald Troyer <ron.troyer@gmail.com>
2020-11-10 13:53:31 -0500

    Link to spotify episode url instead of direct mp3 (#3)
    Co-authored-by: Troyer, Ronald <ronald.troyer@chase.com>

--- spotifeed.rb
+++ spotifeed.rb
@@ -69,7 +69,7 @@ class Spotifeed < Sinatra::Base
           item.itunes_image = episode.dig('images', 0, 'url')
           item.itunes_explicit = episode['explicit']
 
-          item.link = "https://anon-podcast.scdn.co/#{episode['audio_preview_url'].split('/').last}"
+          item.link = episode.dig('external_urls', 'spotify')
           item.enclosure.url = "https://anon-podcast.scdn.co/#{episode['audio_preview_url'].split('/').last}"
           item.enclosure.length = (episode['duration_ms'] / 1000).floor()
           item.enclosure.type = 'audio/mpeg'
