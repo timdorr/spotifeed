4e430ee97d8a509a710ac88bb4abb0152b07b7b6
Tim Dorr <git@timdorr.com>
2020-12-03 16:10:34 -0500

    Add duration and language info

--- spotifeed.rb
+++ spotifeed.rb
@@ -43,13 +43,14 @@ class Spotifeed < Sinatra::Base
     return 'Not a valid show' if show['error']
 
     content_type 'application/rss+xml; charset=utf-8'
-    RSS::Maker.make('rss2.0') do |rss|
+    RSS::Maker::RSS20.make do |rss|
       rss.channel.title = show['name']
       rss.channel.description = show['description']
       rss.channel.itunes_summary = show['description']
       rss.channel.link = show.dig('external_urls', 'spotify')
       rss.channel.author = show['publisher']
       rss.channel.itunes_author = show['publisher']
+      rss.channel.language = show['languages'].first
 
       rss.image.url = show.dig('images', 0, 'url')
       rss.image.title = show['name']
@@ -61,17 +62,20 @@ class Spotifeed < Sinatra::Base
       rss.items.do_sort = true
       show.dig('episodes', 'items').each do |episode|
         rss.items.new_item do |item|
+          duration_secs = (episode['duration_ms'] / 1000).floor
+
           item.guid.content = episode['uri']
           item.title = episode['name']
           item.description = episode['description']
           item.date = Time.parse(episode['release_date'] || '01-01-2020').to_s
 
           item.itunes_image = episode.dig('images', 0, 'url')
+          item.itunes_duration = "%02d:%02d:%02d" % [duration_secs / 3600, duration_secs / 60 % 60, duration_secs % 60]
           item.itunes_explicit = episode['explicit']
 
           item.link = episode.dig('external_urls', 'spotify')
           item.enclosure.url = "https://anon-podcast.scdn.co/#{episode['audio_preview_url'].split('/').last}"
-          item.enclosure.length = (episode['duration_ms'] / 1000).floor()
+          item.enclosure.length = duration_secs
           item.enclosure.type = 'audio/mpeg'
         end
       end
